9be0b5ab8a5e990fcf641128276c57334d6f2ef8
QiFen.Pocket <QiFen.Pocket@gmail.com>
2020-02-29 00:14:14 +0800

    test: add new case

--- CSharpAdvanceDesignTests/GroupSum.cs
+++ CSharpAdvanceDesignTests/GroupSum.cs
@@ -8,7 +8,7 @@ namespace CSharpAdvanceDesignTests
     public class GroupSumTests
     {
         [Test]
-        public void group_sum_of_saving()
+        public void group_sum_of_3_saving()
         {
             var accounts = new[]
             {
@@ -31,6 +31,67 @@ namespace CSharpAdvanceDesignTests
             var expected = new[] {60, 150, 240, 210};
 
             expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        [Test]
+        public void group_sum_of_4_saving()
+        {
+            var accounts = new[]
+            {
+                new Account {Name = "Joey", Saving = 10},
+                new Account {Name = "David", Saving = 20},
+                new Account {Name = "Tom", Saving = 30},
+                new Account {Name = "Joseph", Saving = 40},
+                new Account {Name = "Jackson", Saving = 50},
+                new Account {Name = "Terry", Saving = 60},
+                new Account {Name = "Mary", Saving = 70},
+                new Account {Name = "Peter", Saving = 80},
+                new Account {Name = "Jerry", Saving = 90},
+                new Account {Name = "Martin", Saving = 100},
+                new Account {Name = "Bruce", Saving = 110}
+            };
+
+            var actual = JoeyGroup4Sum(accounts);
+
+            var expected = new[] {100, 260, 300};
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        private IEnumerable<int> JoeyGroup4Sum(IEnumerable<Account> accounts)
+        {
+            var groups = new Dictionary<int, List<Account>>();
+
+            var index = 0;
+
+            foreach (var account in accounts)
+            {
+                var groupKey = index / 4;
+                if (!groups.ContainsKey(groupKey))
+                {
+                    groups[groupKey] = new List<Account>();
+                }
+
+                groups[groupKey].Add(account);
+
+                index++;
+            }
+
+            var sums = new List<int>();
+
+            foreach (var group in groups)
+            {
+                var sum = 0;
+
+                foreach (var account in group.Value)
+                {
+                    sum += account.Saving;
+                }
+
+                sums.Add(sum);
+            }
+
+            return sums;
         }
 
         private IEnumerable<int> JoeyGroupSum(IEnumerable<Account> accounts)
