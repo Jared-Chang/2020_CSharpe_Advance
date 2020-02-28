4fc91962c26532beb681f1d22e649be976f8b16e
QiFen.Pocket <QiFen.Pocket@gmail.com>
2020-02-28 17:15:08 +0800

    refactor: add new case to narrow architecure

--- CSharpAdvanceDesignTests/JoeyTakeWhileTests.cs
+++ CSharpAdvanceDesignTests/JoeyTakeWhileTests.cs
@@ -33,6 +33,25 @@ namespace CSharpAdvanceDesignTests
             expected.ToExpectedObject().ShouldMatch(actual);
         }
 
+        [Test]
+        public void take_cards_until_normal_card()
+        {
+            var cards = new List<Card>
+            {
+                new Card {Kind = CardKind.Normal, Point = 2},
+                new Card {Kind = CardKind.Normal, Point = 3},
+                new Card {Kind = CardKind.Normal, Point = 4},
+                new Card {Kind = CardKind.Separate},
+                new Card {Kind = CardKind.Normal, Point = 5},
+                new Card {Kind = CardKind.Normal, Point = 6}
+            };
+
+            var actual = JoeyTakeWhileForSeparate(cards);
+
+            var expected = new List<Card>();
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
         private IEnumerable<Card> JoeyTakeWhile(IEnumerable<Card> cards)
         {
             using var enumerator = cards.GetEnumerator();
@@ -40,12 +59,30 @@ namespace CSharpAdvanceDesignTests
             while (enumerator.MoveNext())
             {
                 var current = enumerator.Current;
-                if (current.Kind != CardKind.Normal)
+                if (current.Kind == CardKind.Normal)
                 {
-                    yield break;
+                    yield return current;
+                    continue;
                 }
 
-                yield return current;
+                yield break;
+            }
+        }
+
+        private IEnumerable<Card> JoeyTakeWhileForSeparate(IEnumerable<Card> cards)
+        {
+            using var enumerator = cards.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                var current = enumerator.Current;
+                if (current.Kind == CardKind.Separate)
+                {
+                    yield return current;
+                    continue;
+                }
+
+                yield break;
             }
         }
     }
