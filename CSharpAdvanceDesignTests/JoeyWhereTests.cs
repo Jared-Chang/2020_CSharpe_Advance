1c075e97ca098fda418c76031b2f474a6899b9ac
QiFen.Pocket <QiFen.Pocket@gmail.com>
2020-02-28 10:53:17 +0800

    refactor(Add extension method 2): move to file

--- CSharpAdvanceDesignTests/JoeyWhereTests.cs
+++ CSharpAdvanceDesignTests/JoeyWhereTests.cs
@@ -1,29 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using ExpectedObjects;
+using Lab;
 using Lab.Entities;
 using NUnit.Framework;
 
 namespace CSharpAdvanceDesignTests
 {
-    public class LinqExtensions
-    {
-        public static List<TSource> JoeyWhere<TSource>(List<TSource> source, Predicate<TSource> predicate)
-        {
-            var list = new List<TSource>();
-
-            foreach (var item in source)
-            {
-                if (predicate(item))
-                {
-                    list.Add(item);
-                }
-            }
-
-            return list;
-        }
-    }
-
     [TestFixture]
     public class JoeyWhereTests
     {
---
+++ Lab/LinqExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab
+{
+    public class LinqExtensions
+    {
+        public static List<TSource> JoeyWhere<TSource>(List<TSource> source, Predicate<TSource> predicate)
+        {
+            var list = new List<TSource>();
+
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                {
+                    list.Add(item);
+                }
+            }
+
+            return list;
+        }
+    }
+}
