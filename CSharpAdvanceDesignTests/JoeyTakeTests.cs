fb7e63cb1f6f80c9684b804004ccfa0ebfbafbad
QiFen.Pocket <QiFen.Pocket@gmail.com>
2020-02-28 16:16:53 +0800

    test: into take section

--- CSharpAdvanceDesignTests/JoeyTakeTests.cs
+++ CSharpAdvanceDesignTests/JoeyTakeTests.cs
@@ -1,13 +1,11 @@
-﻿using ExpectedObjects;
+﻿using System.Collections.Generic;
+using ExpectedObjects;
 using Lab.Entities;
 using NUnit.Framework;
-using NUnit.Framework.Internal;
-using System.Collections.Generic;
 
 namespace CSharpAdvanceDesignTests
 {
-    [TestFixture()]
-    [Ignore("not yet")]
+    [TestFixture]
     public class JoeyTakeTests
     {
         [Test]
@@ -20,15 +18,10 @@ namespace CSharpAdvanceDesignTests
             var expected = new List<Employee>
             {
                 new Employee {FirstName = "Joey", LastName = "Chen"},
-                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "Tom", LastName = "Li"}
             };
 
-            expected.ToExpectedObject().ShouldEqual(actual);
-        }
-
-        private IEnumerable<Employee> JoeyTake(IEnumerable<Employee> employees)
-        {
-            throw new System.NotImplementedException();
+            expected.ToExpectedObject().ShouldMatch(actual);
         }
 
         private static IEnumerable<Employee> GetEmployees()
@@ -39,8 +32,28 @@ namespace CSharpAdvanceDesignTests
                 new Employee {FirstName = "Tom", LastName = "Li"},
                 new Employee {FirstName = "David", LastName = "Chen"},
                 new Employee {FirstName = "Mike", LastName = "Chang"},
-                new Employee {FirstName = "Joseph", LastName = "Yao"},
+                new Employee {FirstName = "Joseph", LastName = "Yao"}
             };
+        }
+
+        private IEnumerable<Employee> JoeyTake(IEnumerable<Employee> employees)
+        {
+            var index = 0;
+            using var enumerator = employees.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                if (index < 2)
+                {
+                    yield return enumerator.Current;
+                }
+                else
+                {
+                    yield break;
+                }
+
+                index++;
+            }
         }
     }
 }
