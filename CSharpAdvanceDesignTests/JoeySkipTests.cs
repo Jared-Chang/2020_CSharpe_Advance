794c75825e115ab95a58372b440acb59bbb1f50b
QiFen.Pocket <QiFen.Pocket@gmail.com>
2020-02-28 16:56:35 +0800

    test: new case to narrow requirement

--- CSharpAdvanceDesignTests/JoeySkipTests.cs
+++ CSharpAdvanceDesignTests/JoeySkipTests.cs
@@ -33,9 +33,11 @@ namespace CSharpAdvanceDesignTests
             expected.ToExpectedObject().ShouldEqual(actual.ToList());
         }
 
-        private static IEnumerable<Employee> GetEmployees()
+
+        [Test]
+        public void skip_3_employees()
         {
-            return new List<Employee>
+            var employees = (IEnumerable<Employee>) new List<Employee>
             {
                 new Employee {FirstName = "Joey", LastName = "Chen"},
                 new Employee {FirstName = "Tom", LastName = "Li"},
@@ -43,6 +45,16 @@ namespace CSharpAdvanceDesignTests
                 new Employee {FirstName = "Mike", LastName = "Chang"},
                 new Employee {FirstName = "Joseph", LastName = "Yao"}
             };
+
+            var actual = JoeySkip3(employees);
+
+            var expected = new List<Employee>
+            {
+                new Employee {FirstName = "Mike", LastName = "Chang"},
+                new Employee {FirstName = "Joseph", LastName = "Yao"}
+            };
+
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
         }
 
         private IEnumerable<Employee> JoeySkip(IEnumerable<Employee> employees)
@@ -51,6 +63,26 @@ namespace CSharpAdvanceDesignTests
 
             var index = 0;
             var count = 2;
+
+            while (enumerator.MoveNext()
+            )
+            {
+                if (index < count)
+                {
+                    index++;
+                    continue;
+                }
+
+                yield return enumerator.Current;
+            }
+        }
+
+        private IEnumerable<Employee> JoeySkip3(IEnumerable<Employee> employees)
+        {
+            using var enumerator = employees.GetEnumerator();
+
+            var index = 0;
+            var count = 3;
 
             while (enumerator.MoveNext()
             )
