28b8b25725b7ab3730a3139057762a030008dfcc
QiFen.Pocket <QiFen.Pocket@gmail.com>
2020-02-29 11:54:37 +0800

    test: add case and extract predicate and inline

--- CSharpAdvanceDesignTests/JoeyLastTests.cs
+++ CSharpAdvanceDesignTests/JoeyLastTests.cs
@@ -11,6 +11,23 @@ namespace CSharpAdvanceDesignTests
     public class JoeyLastTests
     {
         [Test]
+        public void get_last_employee()
+        {
+            var employees = new List<Employee>
+            {
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "David", LastName = "Chen"},
+                new Employee {FirstName = "Cash", LastName = "Li"}
+            };
+
+            var employee = JoeyLast(employees, employee1 => true);
+
+            new Employee {FirstName = "Cash", LastName = "Li"}
+                .ToExpectedObject().ShouldMatch(employee);
+        }
+
+        [Test]
         public void get_last_chen()
         {
             var employees = new List<Employee>
@@ -21,9 +38,9 @@ namespace CSharpAdvanceDesignTests
                 new Employee {FirstName = "Cash", LastName = "Li"}
             };
 
-            var employee = JoeyLast(employees);
+            var employee = JoeyLast(employees, current => current.LastName == "Chen");
 
-            new Employee {FirstName = "Cash", LastName = "Li"}
+            new Employee {FirstName = "David", LastName = "Chen"}
                 .ToExpectedObject().ShouldMatch(employee);
         }
 
@@ -34,18 +51,21 @@ namespace CSharpAdvanceDesignTests
             {
             };
 
-            TestDelegate action = () => JoeyLast(employees);
+            TestDelegate action = () => JoeyLast(employees, employee => true);
             Assert.Throws<InvalidOperationException>(action);
         }
 
-        private Employee JoeyLast(IEnumerable<Employee> employees)
+        private Employee JoeyLast(IEnumerable<Employee> employees, Func<Employee, bool> predicate)
         {
             using var enumerator = employees.Reverse().GetEnumerator();
             while (enumerator.MoveNext())
             {
                 var current = enumerator.Current;
 
-                return current;
+                if (predicate(current))
+                {
+                    return current;
+                }
             }
 
             throw new InvalidOperationException($"{nameof(employees)} is no matched result");
