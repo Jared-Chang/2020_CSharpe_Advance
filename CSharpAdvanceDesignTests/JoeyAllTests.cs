addb161292c3f63d979da64d4d71024f7ea81393
QiFen.Pocket <QiFen.Pocket@gmail.com>
2020-02-29 10:28:20 +0800

    test: into all

--- CSharpAdvanceDesignTests/JoeyAllTests.cs
+++ CSharpAdvanceDesignTests/JoeyAllTests.cs
@@ -1,23 +1,22 @@
-﻿using Lab.Entities;
+﻿using System.Collections.Generic;
+using Lab.Entities;
 using NUnit.Framework;
-using System.Collections.Generic;
 
 namespace CSharpAdvanceDesignTests
 {
-    [Ignore("not yet")]
     [TestFixture]
     public class JoeyAllTests
     {
         [Test]
-        public void girls_all_adult()
+        public void girls_age_greater_than_18()
         {
             var girls = new List<Girl>
             {
-                new Girl{Age = 20},
-                new Girl{Age = 21},
-                new Girl{Age = 17},
-                new Girl{Age = 18},
-                new Girl{Age = 30},
+                new Girl {Age = 20},
+                new Girl {Age = 21},
+                new Girl {Age = 17},
+                new Girl {Age = 18},
+                new Girl {Age = 30}
             };
 
             var actual = JoeyAll(girls);
@@ -26,7 +25,15 @@ namespace CSharpAdvanceDesignTests
 
         private bool JoeyAll(IEnumerable<Girl> girls)
         {
-            throw new System.NotImplementedException();
+            foreach (var girl in girls)
+            {
+                if (!(girl.Age >= 18))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
