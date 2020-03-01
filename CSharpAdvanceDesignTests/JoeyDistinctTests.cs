1fc37753ec3e27a773e7ab2f30cbc5c7e29a8767
QiFen.Pocket <QiFen.Pocket@gmail.com>
2020-03-01 13:36:21 +0800

    test: distinct

--- CSharpAdvanceDesignTests/JoeyDistinctTests.cs
+++ CSharpAdvanceDesignTests/JoeyDistinctTests.cs
@@ -1,28 +1,26 @@
-﻿using ExpectedObjects;
+﻿using System.Collections.Generic;
+using ExpectedObjects;
 using NUnit.Framework;
-using NUnit.Framework.Internal;
-using System.Collections.Generic;
 
 namespace CSharpAdvanceDesignTests
 {
-    [TestFixture()]
-    [Ignore("not yet")]
+    [TestFixture]
     public class JoeyDistinctTests
     {
         [Test]
         public void distinct_numbers()
         {
-            var numbers = new[] { 91, 3, 91, -1 };
+            var numbers = new[] {91, 3, 91, -1};
             var actual = Distinct(numbers);
 
-            var expected = new[] { 91, 3, -1 };
+            var expected = new[] {91, 3, -1};
 
             expected.ToExpectedObject().ShouldMatch(actual);
         }
 
         private IEnumerable<int> Distinct(IEnumerable<int> numbers)
         {
-            throw new System.NotImplementedException();
+            return new HashSet<int>(numbers);
         }
     }
 }
