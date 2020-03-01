36541c881bbcbc916e7d3b78d1c377f44f4ca28c
QiFen.Pocket <QiFen.Pocket@gmail.com>
2020-03-01 15:14:35 +0800

    feat: intersect

--- CSharpAdvanceDesignTests/JoeyIntersectTests.cs
+++ CSharpAdvanceDesignTests/JoeyIntersectTests.cs
@@ -1,29 +1,39 @@
-﻿using ExpectedObjects;
+﻿using System.Collections.Generic;
+using ExpectedObjects;
 using NUnit.Framework;
-using System.Collections.Generic;
 
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeyIntersectTests
     {
         [Test]
         public void intersect_numbers()
         {
-            var first = new[] { 1, 3, 5 };
-            var second = new[] { 5, 7, 3 };
+            var first = new[] {1, 3, 5, 3};
+            var second = new[] {5, 7, 3, 7};
 
             var actual = JoeyIntersect(first, second);
 
-            var expected = new[] { 3, 5 };
+            var expected = new[] {3, 5};
 
             expected.ToExpectedObject().ShouldMatch(actual);
         }
 
         private IEnumerable<int> JoeyIntersect(IEnumerable<int> first, IEnumerable<int> second)
         {
-            throw new System.NotImplementedException();
+            var secondHashSet = new HashSet<int>(second);
+
+            using var secondEnumerator = first.GetEnumerator();
+            while (secondEnumerator.MoveNext())
+            {
+                var current = secondEnumerator.Current;
+
+                if (secondHashSet.Remove(current))
+                {
+                    yield return current;
+                }
+            }
         }
     }
 }
