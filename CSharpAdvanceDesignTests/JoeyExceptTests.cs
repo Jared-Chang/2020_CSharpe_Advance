e0e59ac5103ab67a9805f682257e27aeda31a6d7
QiFen.Pocket <QiFen.Pocket@gmail.com>
2020-03-01 15:21:48 +0800

    feat: except

--- CSharpAdvanceDesignTests/JoeyExceptTests.cs
+++ CSharpAdvanceDesignTests/JoeyExceptTests.cs
@@ -1,28 +1,50 @@
-﻿using ExpectedObjects;
+﻿using System.Collections.Generic;
+using ExpectedObjects;
 using NUnit.Framework;
-using System.Collections.Generic;
 
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeyExceptTests
     {
         [Test]
         public void except_numbers()
         {
-            var first = new[] { 1, 3, 5, 7 };
-            var second = new[] { 7, 1, 4 };
+            var first = new[] {1, 3, 5, 7, 3};
+            var second = new[] {7, 1, 4, 1};
 
             var actual = JoeyExcept(first, second);
-            var expected = new[] { 3, 5 };
+            var expected = new[] {3, 5};
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        [Test]
+        public void except_numbers_2()
+        {
+            var first = new[] {1, 3, 5, 7, 3};
+            var second = new[] {7, 1, 4, 1};
+
+            var actual = JoeyExcept(second, first);
+            var expected = new[] {4};
 
             expected.ToExpectedObject().ShouldMatch(actual);
         }
 
         private IEnumerable<int> JoeyExcept(IEnumerable<int> first, IEnumerable<int> second)
         {
-            throw new System.NotImplementedException();
+            var hashSet = new HashSet<int>(second);
+
+            using var firstEnumerator = first.GetEnumerator();
+            while (firstEnumerator.MoveNext())
+            {
+                var current = firstEnumerator.Current;
+
+                if (hashSet.Add(current))
+                {
+                    yield return current;
+                }
+            }
         }
     }
 }
