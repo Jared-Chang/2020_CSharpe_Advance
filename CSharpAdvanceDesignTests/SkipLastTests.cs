e17498cc5c4e82c88fc07c1254e0b3f8db7a88a9
Jared.Chang <Jared.Chang@titansoft.com.sg>
2020-03-03 15:19:41 +0800

    test: skip last

---
+++ CSharpAdvanceDesignTests/SkipLastTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using ExpectedObjects;
+using NUnit.Framework;
+
+namespace CSharpAdvanceDesignTests
+{
+    [TestFixture]
+    public class SkipLastTests
+    {
+        private IEnumerable<int> JoeySkipLast(IEnumerable<int> numbers, int count)
+        {
+            var queue = new Queue<int>();
+
+            using var enumerator = numbers.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                if (queue.Count == count)
+                {
+                    do
+                    {
+                        yield return queue.Dequeue();
+                        queue.Enqueue(enumerator.Current);
+                    } while (enumerator.MoveNext());
+                }
+                else
+                {
+                    queue.Enqueue(enumerator.Current);
+                }
+            }
+        }
+
+        [Test]
+        public void skip_last_2()
+        {
+            var numbers = new[] {10, 20, 30, 40, 50};
+            var actual = JoeySkipLast(numbers, 2);
+
+            var expected = new[] {10, 20, 30};
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+    }
+}
